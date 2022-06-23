f6c4ec6e3c612dc8efc95bfbfdd96ffaaa59463e
shubham <shubhamaggarwal3964@gmail.com>
2022-06-23 17:46:48 +0530

    my changes

---
+++ shubham.py
@@ -0,0 +1,11 @@
+x = input("enter the string: ")
+lower_case=lambda s: s.lower()
+print(lower_case)
+
+x = input("enter the string: ")
+lower_case=lambda s:s.lower()
+print(lower_case)
+
+x = input("enter the string: ")
+lower_case=map(lambda s:s.lower(),item)
+print(lower_case)
